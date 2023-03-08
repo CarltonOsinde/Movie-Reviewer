59d088088a8d860f026238a8aa0acfc37ae4d21e
Carlton Osinde <carltonosinde@gmail.com>
2023-03-07 21:06:44 -0500

    server code added

---
+++ server/src/test/java/com/example/moviereviewer/MoviereviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviereviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
